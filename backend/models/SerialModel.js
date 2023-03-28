82e72cb869555d597c45094fac8694b54fc95c7f
FrostJax <fjax4505@gmail.com>
2023-03-28 11:13:57 +0800

    Update Backend

---
+++ backend/models/SerialModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const SerialSchema = new Schema(
+    {
+        userID: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        serialNo: {
+            type: Number,
+            required: true,
+        },
+        remarkName: {
+            type: String,
+            required: true,
+        },
+        givenCredit: {
+            type: Number,
+            required: true,
+        },
+        serialStatus: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+)
+
+const Serial = mongoose.model('Serial', SerialSchema)
+module.exports = Serial
