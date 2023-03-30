95750b3d2bcd7e3069d59378f1256e97636329f6
lzx0824 <114902215+lzx0824@users.noreply.github.com>
2023-03-30 14:26:33 +0800

    remove this if fail

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,13 @@ app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
+// Making Build Folder as Public 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/', require('./routes/root'))
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
