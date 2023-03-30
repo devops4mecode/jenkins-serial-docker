2fd3454445982316a523df8b4dd2ba8541a87d47
lzx0824 <114902215+lzx0824@users.noreply.github.com>
2023-03-30 14:28:12 +0800

    Revert "remove this if fail"
    This reverts commit 95750b3d2bcd7e3069d59378f1256e97636329f6.

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,6 @@ app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
-// Making Build Folder as Public 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.use('/', require('./routes/root'))
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
