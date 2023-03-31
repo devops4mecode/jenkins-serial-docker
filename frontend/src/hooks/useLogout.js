19dd53ea3c36ecfe47ab97e9660af4bbd22ed2ee
FrostJax <fjax4505@gmail.com>
2023-03-31 15:20:27 +0800

    Create useLogout.js

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,15 @@
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogout = () => {
+    const { dispatch } = useAuthContext()
+
+    const logout = () => {
+        // Remove the user from local storage
+        localStorage.removeItem('user')
+
+        // Dispatch logout action
+        dispatch({ type: "LOGOUT" })
+    }
+
+    return { logout }
+}
