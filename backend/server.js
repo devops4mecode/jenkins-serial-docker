3f04e98bae2c653296432b8e76eff9b2ec714200
FrostJax <fjax4505@gmail.com>
2023-03-31 13:25:14 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,17 @@ console.log(process.env.NODE_ENV)
 
 connectDB();
 
+// Allow cross-origin requests
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(logger)
-// app.use(cors(corsOptions))
-app.use(cors())
+app.use(cors(corsOptions))
+// app.use(cors())
 app.use(express.json())
 
 app.use(cookieParser())
@@ -27,8 +35,8 @@ app.use('/', express.static(path.join(__dirname, '/public')))
 // Making Build Folder as Public 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use('/', require('./routes/root'))
