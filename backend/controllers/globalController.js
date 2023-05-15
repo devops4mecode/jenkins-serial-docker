d6b83af4e7f847f5259e586d8d77c15de92f65ef
Ribena77 <114893909+Ribena77@users.noreply.github.com>
2023-05-15 09:20:47 +0800

    Revert "Merge remote-tracking branch 'origin/jax' into ribena"
    This reverts commit a23be847da82d5e84a5c2d19862396423de21019, reversing
    changes made to 506c7ad6b13cde01ee401d9dcb5a6c43c1013c3b.

--- backend/controllers/globalController.js
+++ backend/controllers/globalController.js
@@ -30,7 +30,7 @@ const getSerialDetails = async (req, res) => {
         }
         return res.json(foundSerialNo);
     } catch (error) {
-        return res.status(400).json({ error: error.message });
+        return res.status(500).json({ message: "Internal server error" });
     }
 };
 
@@ -75,82 +75,11 @@ const redeemSerials = async (req, res) => {
         }
     } catch (error) {
         console.error(error);
-        return res.status(400).json({ error: error.message });
-    }
-};
-
-const requestSerials = async (req, res) => {
-    try {
-        const { redemptionAcc, serialNo } = req.query;
-    } catch (error) {
-        return res.status(400).json({ error: error.message });
-    }
-};
-
-const generateSerialsByPlayer = async (req, res) => {
-    try {
-        const { givenCredit, amountToGenerate, remarkName, walletBalance } = req.body;
-
-        if (!givenCredit || !amountToGenerate || !remarkName || !walletBalance) {
-            return res.status(400).json({ message: "All fields must be provided" });
-        }
-
-        let serials = [];
-
-        if (walletBalance < (givenCredit*amountToGenerate)){
-            return res.status(400).json("Insufficient balance")
-        }
-
-        // Generate additional serials
-        for (let i = 0; i < amountToGenerate; i++) {
-            serials.push(generateSerialNumber());
-        }
-
-        // Check for duplicates and replace them
-        const anyDuplicate = await Serial.find({ serialNo: { $in: serials } });
-        anyDuplicate.forEach(duplicate => {
-            let newSerial = generateSerialNumber();
-            while (serials.includes(newSerial) || anyDuplicate.map(d => d.serialNo).includes(newSerial)) {
-                newSerial = generateSerialNumber();
-            }
-            serials.splice(serials.indexOf(duplicate.serialNo), 1, newSerial);
-        });
-
-        // Return only the requested number of unique serials
-        serials = serials.slice(0, amountToGenerate);
-
-        const serialDocs = [];
-        for (let j = 0; j < serials.length; j++) {
-            const serialDoc = new Serial({
-                givenCredit,
-                remarkName: "L7." + remarkName,
-                serialNo: serials[j],
-                redemptionAcc: "",
-                serialStatus: true,
-            });
-            // await serialDoc.save();
-            serialDocs.push(serialDoc);
-        }
-        return res.json({ serialDocs });
-    } catch (error) {
-        console.error(error);
         return res.status(500).json({ message: "Internal server error" });
     }
 };
 
-function generateSerialNumber() {
-    let serial = "";
-    const chars = "1234567890";
-
-    for (let i = 0; i < 16; i++) {
-        serial += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return serial;
-}
-
 module.exports = {
-    generateSerialsByPlayer,
     getSerialDetails,
-    redeemSerials,
-    requestSerials
+    redeemSerials
 }
--- backend/cron.js
+++ backend/cron.js
@@ -7,6 +7,7 @@ const Chart = require('./models/ChartModel');
 // Save To
 const Report = require('./models/ReportModel')
 
+
 exports.generateChartData = () => {
     const dashChart = new CronJob('* * * * * *', async function () {
         const yearStart = moment().startOf('year').toDate()
@@ -87,103 +88,73 @@ exports.generateChartData = () => {
 exports.generateSummary = () => {
     // Daily
     const reportSummary = new CronJob('* * * * * *', async function () {
-        const todayStart = moment().startOf('day').toDate()
-        const todayEnd = moment().endOf('day').toDate()
-        // TEST YESTERDAY
-        // const todayStart = moment().startOf('day').subtract(2, 'day').toDate()
-        // const todayEnd = moment().endOf('day').subtract(2, 'day').toDate()
+        const yearStart = moment().startOf('year').toDate()
+        const yearEnd = moment().endOf('year').toDate()
 
         const [
-            overallRedeemedCount,
-            redeemedCount,
-            overallGeneratedCount,
-            topTen,
-            totalAmountRedeemed
+            monthlyRedeemedThroughYear,
+            monthlyGeneratedThroughYear,
         ] = await Promise.all([
-            // overallRedeemedCount
             Serial.aggregate([
-                {
-                    $match: {
-                        serialStatus: false,
-                        updatedAt: { $gte: todayStart, $lte: todayEnd }
-                    }
-                },
+                { $match: { serialStatus: false, updatedAt: { $gte: yearStart, $lte: yearEnd } } },
                 {
                     $group: {
-                        _id: null,
-                        count: { $sum: 1 }
-                    }
-                }
-            ]),
-            // redeemedCount,
-            Serial.aggregate([
-                {
-                    $match: {
-                        serialStatus: false,
-                        updatedAt: { $gte: todayStart, $lte: todayEnd }
+                        _id: {
+                            year: { $year: "$updatedAt" },
+                            month: { $month: "$updatedAt" }
+                        },
+                        givenCredit: { $sum: "$givenCredit" }
                     }
                 },
-                { $group: { _id: "$givenCredit", count: { $sum: 1 } } }
-            ]),
-            // overallGeneratedCount,
-            Serial.aggregate([
                 {
-                    $match: {
-                        createdAt: { $gte: todayStart, $lte: todayEnd }
+                    $project: {
+                        _id: 0,
+                        givenCredit: 1,
+                        month: "$_id.month",
+                        year: "$_id.year",
                     }
                 },
-                { $group: { _id: "$givenCredit", count: { $sum: 1 } } }
+                { $sort: { year: 1, month: 1 } }
             ]),
-            // topTen,
             Serial.aggregate([
-                {
-                    $match: {
-                        serialStatus: false,
-                        updatedAt: { $gte: todayStart, $lte: todayEnd }
-                    }
-                },
+                { $match: { createdAt: { $gte: yearStart, $lte: yearEnd } } },
                 {
                     $group: {
-                        _id: "$redemptionAcc",
-                        count: { $sum: 1 },
-                        totalGivenCredit: { $sum: "$givenCredit" },
+                        _id: {
+                            year: { $year: "$createdAt" },
+                            month: { $month: "$createdAt" },
+                        },
+                        givenCredit: { $sum: "$givenCredit" }
                     }
                 },
-                { $sort: { totalGivenCredit: -1 } },
-                { $limit: 10 }
-            ]),
-            // totalAmountRedeemed
-            Serial.aggregate([
                 {
-                    $match: {
-                        serialStatus: false,
-                        updatedAt: { $gte: todayStart, $lte: todayEnd }
+                    $project: {
+                        _id: 0,
+                        givenCredit: 1,
+                        month: "$_id.month",
+                        year: "$_id.year",
                     }
                 },
-                { $group: { _id: "givenCredit", sum: { $sum: "$givenCredit" } } },
+                { $sort: { year: 1, month: 1 } }
             ]),
         ])
 
-        let summaryData = {
-            overallRedeemedCount: overallRedeemedCount[0]?.count || 0,
-            redeemedCount: redeemedCount.map(({ _id, count }) => ({ amount: _id, count })) || [],
-            overallGeneratedCount: overallGeneratedCount
-                .map(({ _id, count }) => ({ amount: _id, count }))
-                .sort((a, b) => a.amount - b.amount) // Sort by amount in ascending order
-                || [],
-            mostRedeemed: redeemedCount
-                .map(({ _id, count }) => ({ amount: _id, percentage: count / overallRedeemedCount[0]?.count * 100 }))
-                .sort((a, b) => a.amount - b.amount) // Sort by amount in ascending order
-                || [],
-            topTen: topTen.map(({ _id, count, totalGivenCredit }) => ({ name: _id, count: count, totalCredit: totalGivenCredit })),
-            totalAmountRedeemed: totalAmountRedeemed[0]?.sum || 0
-        }
-        const existingSummary = await Report.findOne({ createdAt: { $gte: todayStart, $lte: todayEnd } })
+        const totalGenerated = monthlyGeneratedThroughYear.reduce((acc, curr) => acc + curr.givenCredit, 0);
+        const totalRedeemed = monthlyRedeemedThroughYear.reduce((acc, curr) => acc + curr.givenCredit, 0);
 
-        if (existingSummary) {
-            await Report.updateOne({ _id: existingSummary._id }, summaryData)
+        let chartData = {
+            monthlyRedeemed: monthlyRedeemedThroughYear,
+            monthlyGenerated: monthlyGeneratedThroughYear,
+            totalGenerated,
+            totalRedeemed,
+        }
+
+        const existingChart = await Chart.findOne({ createdAt: { $gte: yearStart, $lte: yearEnd } })
+
+        if (existingChart) {
+            await Chart.updateOne({ _id: existingChart._id }, chartData)
         } else {
-            await Report.create(summaryData)
+            await Chart.create(chartData)
         }
     },
         null,
