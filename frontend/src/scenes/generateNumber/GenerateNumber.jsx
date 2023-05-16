15edcc3df554514454dd1564551642a2d89bdddc
Ribena77 <114893909+Ribena77@users.noreply.github.com>
2023-05-16 12:05:36 +0800

    Update GenerateNumber.jsx

--- frontend/src/scenes/generateNumber/GenerateNumber.jsx
+++ frontend/src/scenes/generateNumber/GenerateNumber.jsx
@@ -82,7 +82,7 @@ const GenerateNumber = () => {
     });
     console.log(data)
     
-    const CSVcreatedAt = new Date(data[0].createdAt);
+    const CSVcreatedAt = new Date(data[0]?.createdAt);
     const CSVformattedDate = `${CSVcreatedAt.getDate().toString().padStart(2, '0')}${(CSVcreatedAt.getMonth() + 1).toString().padStart(2, '0')}${CSVcreatedAt.getFullYear().toString().slice(2)}_${CSVcreatedAt.getHours().toString().padStart(2, '0')}${CSVcreatedAt.getMinutes().toString().padStart(2, '0')}${CSVcreatedAt.getSeconds().toString().padStart(2, '0')}`;
 
     console.log(CSVformattedDate)
@@ -220,7 +220,7 @@ const GenerateNumber = () => {
 
 
                             <Box className="export-button-component">
-                                <CSVLink data={data} headers={headers} filename={`${data[0].remarkName}_${CSVformattedDate}.csv`}>
+                                <CSVLink data={data} headers={headers} filename={`${data[0]?.remarkName}_${CSVformattedDate}.csv`}>
                                     <Button
                                         className="export-button-text"
                                         variant="contained">
