07d73a902957196ce3bcd2686581b7a8874ce3df
FrostJax <fjax4505@gmail.com>
2023-06-22 16:33:08 +0800

    Update Backend

---
+++ backend/utils/timezone.js
@@ -0,0 +1,41 @@
+const moment = require('moment-timezone');
+
+// Set TimeZone to UTC+8
+moment.tz.setDefault('Asia/Singapore')
+
+const startTime = moment().startOf('day').toDate()
+const timeNow = moment().toDate()
+const endTime = moment().endOf('day').toDate();
+const yearStart = moment().startOf('year').toDate()
+const yearEnd = moment().endOf('year').toDate()
+
+// Convert Time To Start Of Day Date Time
+const convertDayStart = (date) => {
+    return moment(date).startOf('day').toDate();
+}
+
+// Convert Time To End Of Day Date Time
+const convertDayEnd = (date) => {
+    return moment(date).endOf('day').toDate();
+}
+
+const convertYearStart = (year) => {
+    return moment(year).startOf('year').toDate()
+}
+
+const convertYearEnd = (year) => {
+    return moment(year).endOf('year').toDate()
+}
+
+
+module.exports = {
+    startTime,
+    timeNow,
+    endTime,
+    yearStart,
+    yearEnd,
+    convertDayStart,
+    convertDayEnd,
+    convertYearStart,
+    convertYearEnd,
+};
